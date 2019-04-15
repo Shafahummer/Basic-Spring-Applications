e3e3c00586f40190b512b638a2863d5f79498593
DELL <DELL@DELL-PC>
2019-04-15 22:02:28 +0530

    Spring_AOP_Basics

---
+++ Spring_AOP/src/main/java/com/Spring_AOP/AppConfig.java
@@ -0,0 +1,11 @@
+package com.Spring_AOP;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="com.Spring_AOP")
+public class AppConfig
+{
+	
+}
---
+++ Spring_AOP/src/main/java/com/Spring_AOP/MyClass.java
@@ -0,0 +1,14 @@
+package com.Spring_AOP;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyClass 
+{
+	
+	public void show()
+	{
+		
+		System.out.println("Hai Everyone!!!");
+	}
+}
